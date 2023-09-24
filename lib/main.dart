8721896550e3743c57665b7f4f61417ea6da2009
nazimfeni <mdnazimuddin77@gmail.com>
2023-09-24 23:34:06 +0600

    Button style updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-void main(){
+import 'package:flutter_module07_livetest/style.dart';
 
+void main() {
   runApp(MyApp());
 }
 
@@ -15,7 +16,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
@@ -24,7 +24,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int counter =0;
+  int counter = 0;
   Future<void> _showCongratulationsDialog(counter) async {
     return showDialog<void>(
       context: context,
@@ -32,58 +32,63 @@ class _HomePageState extends State<HomePage> {
         return AlertDialog(
           title: Text('Button pressed ${counter} times'),
           actions: <Widget>[
-            TextButton(onPressed: (){
-              Navigator.pop(context);
-
-            }, child: Text("Close"))
+            TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Close"))
           ],
         );
       },
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Counter App"),),
+      appBar: AppBar(
+        title: Text("Counter App"),
+      ),
       body: Center(
-        
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Count:", style: TextStyle(fontSize: 30),),
-            Text(counter.toString(), style: TextStyle(fontSize: 70, fontWeight: FontWeight.bold),),
+            Text(
+              "Count:",
+              style: TextStyle(fontSize: 30),
+            ),
+            Text(
+              counter.toString(),
+              style: TextStyle(fontSize: 70, fontWeight: FontWeight.bold),
+            ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                ElevatedButton(onPressed: (){
-
-                  setState(() {
-                    counter = counter+1;
-                    if (counter >= 5) {
-                      _showCongratulationsDialog(counter);
-                    }
-
-
-
-                  });
-                }, child: Icon(Icons.add)),
+                ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        counter = counter + 1;
+                        if (counter >= 5) {
+                          _showCongratulationsDialog(counter);
+                        }
+                      });
+                    },
+                    child: Icon(Icons.add), style: AppButtonStyle(),),
                 SizedBox(width: 30),
-                ElevatedButton(onPressed: (){
-
-                  setState(() {
-                    if (counter > 0) {
-                      counter = counter - 1;
-                    }
-                  });
-                }, child: Icon(Icons.remove)),
+                ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        if (counter > 0) {
+                          counter = counter - 1;
+                        }
+                      });
+                    },
+                    child: Icon(Icons.remove),style: AppButtonStyle(),),
               ],
             )
           ],
         ),
-        
-        
       ),
-
     );
   }
 }
---
+++ lib/style.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+ButtonStyle AppButtonStyle(){
+  return ElevatedButton.styleFrom(
+    padding: EdgeInsets.symmetric(horizontal: 30.0),
+  );
+
+}
